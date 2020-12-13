e752bebef45d1cb7bfc23cf76dec919a7c0ebf19
Stepan Burduzha <murstepan26@gmail.com>
2020-12-13 18:33:36 +0300

    second commit

--- app/src/main/java/com/example/recipebook/MainScreen.java
+++ app/src/main/java/com/example/recipebook/MainScreen.java
@@ -1,15 +1,52 @@
 package com.example.recipebook;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainScreen extends AppCompatActivity {
+
+    private DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+        myRef = FirebaseDatabase.getInstance().getReference();
+        findViewById(R.id.test_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference ref = db.getReference("message"); // Key
+                ref.setValue("This is a test message"); // Value
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    public void onDataWrite(View view) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference ref = db.getReference("message"); // Key
+        ref.setValue("This is a test message"); // Value
+
     }
 }
+
